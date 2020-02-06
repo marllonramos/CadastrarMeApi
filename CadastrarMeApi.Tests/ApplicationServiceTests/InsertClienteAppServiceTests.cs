86e0332ab97d0de770bf5ba1a0025844551f6100
marllonramos <87marllon@gmail.com>
2020-02-06 19:26:08 -0300

    Implementando testes

---
+++ CadastrarMeApi.Tests/ApplicationServiceTests/InsertClienteAppServiceTests.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CadastrarMeApi.Tests.ApplicationServiceTests
+{
+    [TestClass]
+    public class InsertClienteAppServiceTests
+    {
+        [TestMethod]
+        public void Dado_um_comando_invalido_deve_interromper_a_execucao()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Dado_um_comando_valido_deve_interromper_a_execucao()
+        {
+            Assert.Fail();
+        }
+    }
+}
